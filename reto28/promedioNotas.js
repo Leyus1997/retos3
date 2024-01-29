c4d41d7a3ca9ebae63489f68d02dafc54a68b2ab
Leyus1997 <jonathanbryancueva@gmail.com>
2024-01-28 21:07:26 -0500

    Se agrega la funcion if e imagenes de resultado

--- reto28/promedioNotas.js
+++ reto28/promedioNotas.js
@@ -1,9 +1,16 @@
 calcularPromedioNotas=function(){
-    let nota1,nota2,nota3,promedio,resultadoFormateado,resultado;
+    let nota1,nota2,nota3,promedio,resultadoFormateado,resultado,resultadoImg;
     nota1=recuperarFlotante("txtN1");
     nota2=recuperarFlotante("txtN2");
     nota3=recuperarFlotante("txtN3");
     promedio=calcularPromedio(nota1,nota2,nota3);
     resultadoFormateado=promedio.toFixed(2);
     
+    if (resultadoFormateado>6.99) {
+        resultadoImg=cambiarImagen("imgResultado","exito.gif");
+        resultado=cambiarTexto("lblResultado",resultadoFormateado);
+    } else {
+        resultadoImg=cambiarImagen("imgResultado","fracaso.gif");
+        resultado=cambiarTexto("lblResultado",resultadoFormateado);
+    }
 }
