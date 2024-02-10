502c2082b7204fcf2a4815a07667849038ccc348
Leyus1997 <jonathanbryancueva@gmail.com>
2024-02-10 13:36:00 -0500

    Ejercicio terminado y pruebas de botones validar y limpiar

--- reto_35_placa/placas.js
+++ reto_35_placa/placas.js
@@ -4,8 +4,17 @@ validarPlaca=function(){
     erroresEstructura=vaildarEstructura(placa);
      
      if (erroresEstructura == null) {
-        mostrarTexto("lblError1","Estructura Valida");//  *****
+        mostrarTexto("lblError1","Estructura Valida");
+        obtenerProvincia(placa);
+        obtenerTipoVehiculo(placa);
+        obternerDiaPicoYPlaca(placa);
      } else {
         alert ("Estructura Incorrecta");
      }
+}
+limpiar=function(){
+    mostrarTexto("lblError1"," ");
+    mostrarTexto("lblError2"," ");
+    mostrarTexto("lblError3"," ");
+    mostrarTexto("lblError4"," ");
 }
--- reto_35_placa/serviciosPlacas.js
+++ reto_35_placa/serviciosPlacas.js
@@ -50,6 +50,95 @@ vaildarEstructura = function(placa){
     }
     return null;
 }
+obtenerProvincia=function(placa){
+    let letra1;
+    letra1 = placa.charAt(0);
+    if (letra1=="A"){
+        mostrarTexto("lblError2","Azuay")
+    } else if(letra1=="B"){
+        mostrarTexto("lblError2","Bolívar")
+    }else if(letra1=="U"){
+        mostrarTexto("lblError2","Cañar")
+    }else if(letra1=="C"){
+        mostrarTexto("lblError2","Carchi")
+    }else if(letra1=="X"){
+        mostrarTexto("lblError2","Cotopaxi")
+    }else if(letra1=="H"){
+        mostrarTexto("lblError2","Chimborazo")
+    }else if(letra1=="O"){
+        mostrarTexto("lblError2","El Oro")
+    }else if(letra1=="E"){
+        mostrarTexto("lblError2","Esmeraldas")
+    }else if(letra1=="W"){
+        mostrarTexto("lblError2","Galápagos")
+    }else if(letra1=="G"){
+        mostrarTexto("lblError2","Guayas")
+    }else if(letra1=="I"){
+        mostrarTexto("lblError2","Imbabura")
+    }else if(letra1=="L"){
+        mostrarTexto("lblError2","Loja")
+    }else if(letra1=="R"){
+        mostrarTexto("lblError2","Los Ríos")
+    }else if(letra1=="M"){
+        mostrarTexto("lblError2","Manabí")
+    }else if(letra1=="V"){
+        mostrarTexto("lblError2","Morona Santiago")
+    }else if(letra1=="N"){
+        mostrarTexto("lblError2","Napo")
+    }else if(letra1=="S"){
+        mostrarTexto("lblError2","Pastaza")
+    }else if(letra1=="P"){
+        mostrarTexto("lblError2","Pichincha")
+    }else if(letra1=="K"){
+        mostrarTexto("lblError2","Sucumbíos")
+    }else if(letra1=="Q"){
+        mostrarTexto("lblError2","Orellana")
+    }else if(letra1=="T"){
+        mostrarTexto("lblError2","Tungurahua")
+    }else if(letra1=="Z"){
+        mostrarTexto("lblError2","Zamora Chinchipe")
+    }else if(letra1=="Y"){
+        mostrarTexto("lblError2","Santa Elena")
+    }else{
+    mostrarTexto("lblError2","Provincia Incorrecta")
+    alert ("Provincia Incorrecta");}
+    return null;
+}
+obtenerTipoVehiculo = function (placa){
+    let letra2;
+    letra2 = placa.charAt(1);
+    if (letra2=="A" || letra2=="Z"){
+        mostrarTexto("lblError3","Vehículos comerciales")
+    } else if(letra2=="E"){
+        mostrarTexto("lblError3","Vehículos gubernamentales")
+    }else if(letra2=="X"){
+        mostrarTexto("lblError3","Vehículos de uso oficial")
+    }else if(letra2=="S"){
+        mostrarTexto("lblError3","Vehículos del gobierno provincial")
+    }else if(letra2=="M"){
+        mostrarTexto("lblError3","Vehículos municipales")
+    } 
+    else{
+        mostrarTexto("lblError3","Vehículo particular")
+    }
+    return null;
+}
+obternerDiaPicoYPlaca=function(placa){
+    //obtener última posición de un string
+    ultimaPosicion = placa.length - 1;
+    num4 = placa.charAt(ultimaPosicion);
+    if (num4==1 || num4==2) {
+        mostrarTexto("lblError4","tiene pico y placa Lunes")
+    }else if(num4==3 || num4==4){
+        mostrarTexto("lblError4","tiene pico y placa Martes")
+    }else if(num4==5 || num4==6){
+        mostrarTexto("lblError4","tiene pico y placa Miercoles")
+    }else if(num4==7 || num4==8){
+        mostrarTexto("lblError4","tiene pico y placa Jueves")
+    }else if(num4==9 || num4==0){
+        mostrarTexto("lblError4","tiene pico y placa Viernes")
+    }
+}
 
 
 
