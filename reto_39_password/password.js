d6971704e0163d9c063d1434a27dab4745e649b3
Leyus1997 <jonathanbryancueva@gmail.com>
2024-02-11 21:14:24 -0500

    reto 39_password terminado

--- reto_39_password/password.js
+++ reto_39_password/password.js
@@ -1,37 +1,50 @@
-validarPassword = function(password){
-    let longitudCadena;
-    //logitud minima de 8 caracteres
-    longitudCadena = password.length;
+validarPassword = function (password) {
+    let longitudCadena = password.length;
+    let existeMayuscula = false;
+    let existeDigito = false;
+    let existeCaracterEspecial = false;
+    let esCorrecto= true;
+    // Longitud mínima de 8 caracteres y máxima de 16 caracteres
+    if (longitudCadena >= 8 && longitudCadena <= 16) {
+        
+        for (let i = 0; i < longitudCadena; i++) {
+            let letra = password.charAt(i);
 
-
-    
-    if(longitudCadena>=8 && longitudCadena<=16){
-        mostrarTexto("lblError1","La contraseña debe tener entre 8 y 16 caracteres")
+            if (esMayuscula(letra)) {
+                existeMayuscula = true;
+            }
+            if (esDigito(letra)) {
+                existeDigito = true;
+            }
+            if (esCaracterEspecial(letra)) {
+                existeCaracterEspecial = true;
+            }
+        }
+        if (!existeMayuscula) {
+            mostrarTexto("lblError1","Debe tener al menos una mayúscula");
+            esCorrecto = false;
+        }
+        if (!existeDigito) {
+            mostrarTexto("lblError1","Debe tener al menos un dígito");
+            esCorrecto = false;
+        }
+        if (!existeCaracterEspecial) {
+            mostrarTexto("lblError1","Debe tener al menos un carácter especial");
+            esCorrecto = false;
+        }
+        
+    } else {
+        mostrarTexto("lblError1","La contraseña debe tener entre 8 y 16 caracteres");
+        esCorrecto = false;
     }
-    //validar Mayuscula
-    else if( esMayuscula(password)){
-        mostrarTexto("lblError1","La contraseña debe tener almenos un digito")
-        return true;
-    }
-    //validar numeros
-    else if( esDigito(password)){
-        mostrarTexto("lblError1","La contraseña debe tener almenos un número")
-        return true;
-    }
-    //validar caracter especial *-_ 
-    else if( esCaracterEspecial(password)){
-        mostrarTexto("lblError1","La contraseñe debe tener almenos un caracter especial como: *,-,_")
-        return true;
-    }
-    return 0;
+    return esCorrecto;
 }
-ejercutarValidacion=function(){
+ejercutarValidacion = function () {
     let estructuraErrores;
     let cadenaPassword;
     cadenaPassword = recuperarTexto("txtPassword")
-    estructuraErrores = validarPassword();
-    
-    if (estructuraErrores==0) {
+    estructuraErrores = validarPassword(cadenaPassword);
+    if (estructuraErrores) {
         mostrarTexto("lblError1","Password correcto");
-    } 
+    }
 }
--- reto_39_password/utililtarioPassword.js
+++ reto_39_password/utililtarioPassword.js
@@ -2,39 +2,44 @@ esMayuscula=function(caracter){
     let codigoAscii;
     codigoAscii = caracter.charCodeAt(0);
     if (codigoAscii >= 65 && codigoAscii<=90) {
-        return !true;
-    } else {
-        return !false;
-    }
-}
-esMayuscula=function(caracter) {
-    let codigoAscii;
-    codigoAscii = caracter.charCodeAt(0);
-    if (codigoAscii >= 65 && codigoAscii <= 90) {
         return true;
     } else {
         return false;
     }
 }
+
 esDigito=function(numero){
     let numeroAscii;
     numeroAscii= numero.charCodeAt(0);
     if (numeroAscii >=48 && numeroAscii<=57) {
-        return !true;
+        return true;
     } else {
-        return !false;
+        return false;
     }
 }
 esCaracterEspecial=function(guion){
-let codigoGuion;
-codigoGuion=guion.charCodeAt(0);
-console.log = codigoGuion;
-if (codigoGuion==45 | codigoGuion==42 |codigoGuion==95) {
-    return !true;
+let codigoAscii;
+codigoAscii=guion.charCodeAt(0);
+console.log = codigoAscii;
+if (codigoAscii==45 | codigoAscii==42 |codigoAscii==95) {
+    return true;
 } else {
-    return !false;
+    return false;
 }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
 contarMayusculas=function(cadena){
     let letra;
     let contadorMayusculas=0;
@@ -46,14 +51,4 @@ contarMayusculas=function(cadena){
     }
     console.log(contadorMayusculas);
 }
-contarMayusculas=function(cadena){
-    let letra;
-    let contadorMayusculas=0;
-    for (let i = 0; i < cadena.length; i++) {
-      letra=cadena.charAt(i);
-      if (esMayuscula(letra)) {
-        contadorMayusculas=contadorMayusculas+1;  //contadorMayusculas++
-      }        
-    }
-    return false;
-}
+
