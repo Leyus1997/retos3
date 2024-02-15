9f559c1313e19e0f639347301c43fb0d1afa55fc
Leyus1997 <jonathanbryancueva@gmail.com>
2024-02-15 00:09:43 -0500

    reto 40 completado

--- reto_40_ahorcado/ahorcado.js
+++ reto_40_ahorcado/ahorcado.js
@@ -26,24 +26,36 @@ guardarPalabra = function () {
 }
 mostrarLetra = function (letra,i) {
     
-    let posicionLetra;
-        posicionLetra = ("div"+i);//captura el div correspondiente al cuadra a mostrar
-        mostrarTexto(posicionLetra,letra);
+        if (i==0) {
+            mostrarTexto("div0",letra)
+        }else if(i==1){
+            mostrarTexto("div1",letra)
+        }else if(i==2){
+            mostrarTexto("div2",letra)
+        }else if(i==3){
+            mostrarTexto("div3",letra)
+        }else if(i==4){
+            mostrarTexto("div4",letra)
+        }
     
 }
 validar = function(letra){
     let cadenaTextoSecreta,letraIterada;
     cadenaTextoSecreta = recuperarTexto("txtSecreta");
-    
+    let letraEncontrada = false;
     for (let i = 0; i <=4; i++) {
         letraIterada=cadenaTextoSecreta.charAt(i);
         if (letraIterada==letra) {
             mostrarLetra(letra,i);
             coincidencias++;
-        }  
+            letraEncontrada = true;
+        }
     }
-    errores++
-    alert ("LA LETRA NO ES PARTE DE LA PALABRA")
+    if (letraEncontrada!=true) {
+        errores++;
+        alert ("LA LETRA NO ES PARTE DE LA PALABRA");
+        mostrarAhorcado(errores);
+    }
 }
 ingresarLetra = function (){
     let cadena;
@@ -52,12 +64,12 @@ ingresarLetra = function (){
     cadena=recuperarTexto("txtLetra");
     if (esMayuscula(cadena)){
         intentos++;
-        mostrarTexto = ("lblIntentos",intentos);
+        
         validar(cadena);
         if (coincidencias==5) {
-            mostrarImagen("ahorcadoImagen",ganador.gif);
-        } else if(intentos>=10){
-            mostrarImagen("ahorcadoImagen",gameOver.gif);
+            mostrarImagen("ahorcadoImagen","./ganador.gif");
+        } else if(intentos>10){
+            mostrarImagen("ahorcadoImagen","./gameOver.gif");
             cadena=mostrarTextoEnCaja("txtSecreta","");
             cadena=mostrarTextoEnCaja("txtLetra","");
         }
@@ -67,25 +79,25 @@ ingresarLetra = function (){
     }
 }
 //Implementacion de graficos
-mostrarAhorcado = function(){
+mostrarAhorcado = function(errores){
     if (errores==1) {
-        mostrarImagen("ahorcadoImagen",Ahorcado_01.png);
+        mostrarImagen("ahorcadoImagen","./Ahorcado_01.png");
     } else if(errores==2){
-        mostrarImagen("ahorcadoImagen",Ahorcado_03.png);
+        mostrarImagen("ahorcadoImagen","./Ahorcado_02.png");
     }else if(errores==3){
-        mostrarImagen("ahorcadoImagen",Ahorcado_03.png);
+        mostrarImagen("ahorcadoImagen","./Ahorcado_03.png");
     }else if(errores==4){
-        mostrarImagen("ahorcadoImagen",Ahorcado_04.png);
+        mostrarImagen("ahorcadoImagen","./Ahorcado_04.png");
     }else if(errores==5){
-        mostrarImagen("ahorcadoImagen",Ahorcado_05.png);
+        mostrarImagen("ahorcadoImagen","./Ahorcado_05.png");
     }else if(errores==6){
-        mostrarImagen("ahorcadoImagen",Ahorcado_06.png);
+        mostrarImagen("ahorcadoImagen","./Ahorcado_06.png");
     }else if(errores==7){
-        mostrarImagen("ahorcadoImagen",Ahorcado_07.png);
+        mostrarImagen("ahorcadoImagen","./Ahorcado_07.png");
     }else if(errores==8){
-        mostrarImagen("ahorcadoImagen",Ahorcado_08.png);
+        mostrarImagen("ahorcadoImagen","./Ahorcado_08.png");
     }else if(errores==9){
-        mostrarImagen("ahorcadoImagen",Ahorcado_09.png);
+        mostrarImagen("ahorcadoImagen","./Ahorcado_09.png");
     }
 }
 
