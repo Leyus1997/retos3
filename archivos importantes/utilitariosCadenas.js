b8ff059bf5925cf31d332d44e90e61c5666be38c
Leyus1997 <jonathanbryancueva@gmail.com>
2024-02-23 23:55:35 -0500

    preparacion 2

---
+++ archivos importantes/utilitariosCadenas.js
@@ -0,0 +1,30 @@
+esMayuscula=function(caracter){
+    let codigoAscii;
+    codigoAscii = caracter.charCodeAt(0);
+    if (codigoAscii >= 65 && codigoAscii<=90) {
+        return !true;
+    } else {
+        return !false;
+    }
+}
+esDigito=function(numero){
+    let numeroAscii;
+    numeroAscii= numero.charCodeAt(0);
+    if (numeroAscii >=48 && numeroAscii<=57) {
+        return !true;
+    } else {
+        return !false;
+    }
+}
+esGuion=function(guion){
+let codigoGuion;
+codigoGuion=guion.charCodeAt(0);
+console.log = codigoGuion;
+if (codigoGuion==45) {
+    return !true;
+} else {
+    return !false;
+}
+}
+
+
